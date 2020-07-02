e0211a42f0cf0b874e6a24799f3ee00b44730dcb
Dorival Trindade <32616173+dorivaltrindade@users.noreply.github.com>
2020-07-02 11:11:42 -0300

    aula1

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+alert ("Alerta do JavaScript do main.js!");
+
+var nome = "Dorival";
+var idade = 57;
+var duracao_curso =2;
+var melhor_time = "O Japão possui o melhor time de futebol do mundo!";
+var n1 = 5;
+var n2 = 3;
+
+
+
+alert( nome +" tem "+idade +" anos.");
+alert("Ao terminar o curso, terá "+ (idade + duracao_curso)+" anos!");
+console.log (nome);
+console.log (idade + duracao_curso);
+console.log(melhor_time);
+alert(melhor_time.replace("Japão","Brasil"));
+console.log(melhor_time.toLowerCase());
+console.log("Multiplicando 1º número ("+n1+") * 2º número ("+n2+") = "+n1*n2);
+
+// = = = = = parte 2 = = = = = //
+
